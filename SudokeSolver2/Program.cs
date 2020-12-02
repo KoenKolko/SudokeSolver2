adb5e5957765407dbb582121afefdb95b69af848
Koen Jacobs <koen.jacobswerk@gmail.com>
2020-12-03 00:18:26 +0100

    2

--- SudokeSolver2/Program.cs
+++ SudokeSolver2/Program.cs
@@ -357,15 +357,6 @@ namespace SudokeSolver2
             private List<SudokuPunt> allePunten = new List<SudokuPunt>();
 
             List<int[]> vakIndices = new List<int[]>();
-            int[] indexVak1 = { 1, 2, 3, 10, 11, 12, 19, 20, 21 };
-            int[] indexVak2 = { 4, 5, 6, 13, 14, 15, 22, 23, 24 };
-            int[] indexVak3 = { 7, 8, 9, 16, 17, 18, 25, 26, 27 };
-            int[] indexVak4 = { 28, 29, 30, 37, 38, 39, 46, 47, 48 };
-            int[] indexVak5 = { 31, 32, 33, 40, 41, 42, 49, 50, 51 };
-            int[] indexVak6 = { 34, 35, 36, 43, 44, 45, 52, 53, 54 };
-            int[] indexVak7 = { 55, 56, 57, 64, 65, 66, 73, 74, 75 };
-            int[] indexVak8 = { 58, 59, 60, 67, 68, 69, 76, 77, 78 };
-            int[] indexVak9 = { 61, 62, 63, 70, 71, 72, 79, 80, 81 };
 
             public SudokuVeld()
             {
